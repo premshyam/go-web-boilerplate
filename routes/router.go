5aa9603e6b0791f7ea6e616e9c01d90e52490ff6
Prakalp <prakalpvarshney2@gmail.com>
2023-05-20 02:11:58 -0700

    add controller folder

--- routes/router.go
+++ routes/router.go
@@ -1,17 +1,12 @@
 package router
 
 import (
-	"net/http"
-  
 	"github.com/gin-gonic/gin"
+	"boilerplate/controllers"
   )
-  
+
 func InitializeRoutes(){
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-		})
-	})
+	r.GET("/ping", controller.Pong)
 	r.Run()
 }
