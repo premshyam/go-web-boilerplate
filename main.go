2e77846445a95ba35f217a50d921770d3af3c793
Prakalp <prakalpvarshney2@gmail.com>
2023-05-20 02:04:04 -0700

    add routes in seperate package

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-  "net/http"
-
-  "github.com/gin-gonic/gin"
-)
+import "boilerplate/routes"
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run()
+  router.InitializeRoutes()
 }
